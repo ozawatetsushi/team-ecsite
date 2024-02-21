22addb04787762ce719705819fe57b0996cf3d57
ozawatetsushi <ozawatetsushi@icloud.com>
2024-02-21 14:40:53 +0900

    team-ecsite

---
+++ UserController.java
@@ -0,0 +1,79 @@
+package jp.co.internous.dragon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.co.internous.dragon.model.domain.MstUser;
+import jp.co.internous.dragon.model.form.UserForm;
+import jp.co.internous.dragon.model.mapper.MstUserMapper;
+import jp.co.internous.dragon.model.session.LoginSession;
+
+/**
+ * ユーザー登録に関する処理を行うコントローラー
+ * @author インターノウス
+ *
+ */
+@Controller
+@RequestMapping("/dragon/user")
+public class UserController {
+		
+	/*
+	 * フィールド定義
+	 */
+	@Autowired
+	private MstUserMapper userMapper; 
+	
+	@Autowired
+	private LoginSession loginSession;
+	
+	
+	/**
+	 * 新規ユーザー登録画面を初期表示する。
+	 * @param m 画面表示用オブジェクト
+	 * @return 新規ユーザー登録画面
+	 */
+	@RequestMapping("/")
+	public String index(Model m) {
+		// page_header.htmlでsessionの変数を表示させているため、loginSessionを画面に送る。
+		m.addAttribute("loginSession", loginSession);
+		
+		//新規ユーザー登録pdfを表示
+		return "register_user";
+	}
+	
+	/**
+	 * ユーザー名重複チェックを行う
+	 * @param f ユーザーフォーム
+	 * @return true:重複あり、false:重複なし
+	 */
+	@PostMapping("/duplicatedUserName")
+	@ResponseBody
+	public boolean duplicatedUserName(@RequestBody UserForm f) {
+		
+		int count = userMapper.findCountByUserName(f.getUserName());
+		
+		return count > 0;
+		
+	}
+	
+	/**
+	 * ユーザー情報登録を行う
+	 * @param UserForm ユーザーフォーム
+	 * @return true:登録成功、false:登録失敗
+	 */
+	@PostMapping("/register")
+	@ResponseBody
+	public boolean register(@RequestBody UserForm f) {
+		
+		MstUser user = new MstUser(f);
+		
+		int count = userMapper.insert(user);
+		
+		return count > 0;
+	}
+}
